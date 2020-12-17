3cc19fdb256b0f23659c672cac45292795a051f8
jundymek <jundymek@gmail.com>
2020-12-17 14:29:39 +0100

    feat: calc.controller insert data to database

--- src/calc/calc.controller.ts
+++ src/calc/calc.controller.ts
@@ -1,12 +1,27 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { getConnection } from 'typeorm';
 import { CalcDto } from '../dto/calc/calc-result.dto';
+import { Calc } from './calc.entity';
 import { CalcService } from './calc.service';
 
 @Controller('calc')
 export class CalcController {
   constructor(private calcService: CalcService) {}
   @Post()
-  res(@Body() calcControllerDto: CalcDto) {
+  async res(@Body() calcControllerDto: CalcDto) {
+    await getConnection()
+      .createQueryBuilder()
+      .insert()
+      .into(Calc)
+      .values([
+        {
+          firstCurrency: calcControllerDto.firstCurrency,
+          secondCurrency: calcControllerDto.secondCurrency,
+          amount: calcControllerDto.amount,
+        },
+      ])
+      .execute();
+
     return this.calcService.getComputedValues(
       calcControllerDto.firstCurrency,
       calcControllerDto.secondCurrency,
